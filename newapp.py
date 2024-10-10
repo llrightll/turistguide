e29021c397ffb8838f64c770f8c12e919765b4a1
stihallst <stihallst@gmail.com>
2024-10-10 08:47:17 +0200

    endring

--- newapp.py
+++ newapp.py
@@ -16,7 +16,7 @@ def kall_guide():
         messages=
         [
             {"role": "system", "content": ("Du er en hyggelig turistguide i Oslo og omegn som gir lettleste tips til aktiviteter. Unngå nummererte lister. Bruk en vennlig, uformell og lokal tone som passer for turister i Oslo. Tilpass anbefalingene etter dato og tid på døgnet. Hvis tidspunktet er natt(ikke kveld), si på en vennlig måte at det beste nå er å hvile. Vurder kulturelle, historiske eller moderne attraksjoner basert på brukerens interesse. når du har nevnt steder, skriv en liste på slutten med koordinater for stedene. bruk formatet 'lokasjon, adresse, lat, long'. hvis du ikke har nøyaktig posjisjon, bruk nermeste beregning")},
-            {"role": "user", "content": f"hva kan man gjøre i oslo på {option} om man er interesert i {prompt}. tidsrommet for besøket er {dag} "}
+            {"role": "user", "content": f"hva kan man gjøre i oslo på {option} om man er interesert i {prompt}. tidsrommet for besøket er {dag}. aktuele nyheter om oslo som er formatert med strukturen title, description, content som du skal ha med i beregningen er {st.session_state['combined_articles']}. fortell også om en nyhetssak som er relevant"}
         ]
     )
     st.write(completion.choices[0].message.content)
@@ -24,14 +24,42 @@ def kall_guide():
 
 def get_news():
     startdag = datetime.datetime.now() + datetime.timedelta(days=-10)
-    all_articles = newsapi.get_everything(q='oslo', from_param=startdag)
-    #st.write(all_articles)
+    all_articles = newsapi.get_everything(q='oslo', from_param='2024-09-29')
 
+    if 'news_articles' not in st.session_state:
+        st.session_state['news_articles'] = all_articles
+
+    articles = st.session_state['news_articles'].get('articles', [])
+
+    if 'combined_articles' not in st.session_state:
+        st.session_state['combined_articles'] = []
+
+    for article in articles:
+        title = article.get('title', 'No title')
+        description = article.get('description', 'No description')
+        content = article.get('content', 'No content')
+
+        combined = f"Title: {title}\n\nDescription: {description}\n\nContent: {content}\n{'-'*80}\n"
+
+        #combined = f"Title: {title}\n\nDescription: {description}\n\nContent: {content}\n{'-'*80}\n"
+
+        st.session_state['combined_articles'].append(combined)
+    
+    #for article in st.session_state['combined_articles']:
+        #st.write(article)
+
+
+if 'initialized' not in st.session_state:
+    st.session_state['initialized'] = True
+    get_news()
+
+
+st.write(st.session_state['combined_articles'])
 st.title("Oslo turist guide")
 
 st.write("Hei, hva kan jeg hjelpe med?")
 
-get_news()
+
 
 today = datetime.datetime.now()
 day4 = today + datetime.timedelta(days=4)
